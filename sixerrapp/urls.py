23d60caaeb91751b80b383ff8dfd410e68b07b0e
Jason <>
2017-09-13 10:06:15 -0700

    Create a two pages

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
-
+    url(r'^create_gig/$', views.create_gig, name='create_gig'),
+    url(r'^my_gigs/$', views.my_gigs, name='my_gigs'),
 ]
